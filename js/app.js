4de49a3dc26001cc09f25928d9853718c1ba7661
neternefer <alkarinqe@yahoo.com>
2020-04-14 18:11:10 +0200

    Add initial functionality

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@
  * Define Global Variables
  *
 */
-const menuList = document.getElementById("dynamic-menu-list");
-const linkedSections = document.getElementsByClassName("linked");
+document.addEventListener("DOMContentLoaded", () => {
+    const menuList = document.getElementById("dynamic-menu-list");
+    const linkedSections = document.getElementsByClassName("linked");
+    const headerHeight = 160;
+
 
 
 /**
@@ -11,6 +14,52 @@ const linkedSections = document.getElementsByClassName("linked");
  * Start Helper Functions
  *
 */
+    const populateList = () => {
+        for(section of linkedSections) {
+            let listElement = `
+                <li><a href="#${section.id}" class="${section.id}">
+                    ${section.dataset.nav}
+                    </a>
+                </li>`;
+            menuList.insertAdjacentHTML("beforeend", listElement);
+            menuList.querySelector(`.${section.id}`).addEventListener("click", scrollTo);
+        }
+    }
+
+    const isInViewport = (section) => {
+        let sectionPosition = section.getBoundingClientRect();
+        if(sectionPosition.top <= headerHeight + 0.5 &&
+            sectionPosition.bottom >= headerHeight + 0.5) {
+            return true;
+        }
+        return false;
+    };
+
+    const setActive = () => {
+        for(section of linkedSections){
+            section.classList.toggle('active', isInViewport(section));
+            document.querySelector(`.${section.id}`).classList.toggle('active', isInViewport(section));
+        }
+    };
+
+    const scrollTo = (e) => {
+        console.log(e.target)
+        e.preventDefault();
+        document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+
+    populateList();
+    document.addEventListener("scroll", function(){
+        setActive();
+    });
+
+
+
+
+
+
 
 
 
@@ -40,3 +89,4 @@ const linkedSections = document.getElementsByClassName("linked");
 // Scroll to section on link click
 
 // Set sections as active
+})
