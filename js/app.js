44fcee669247ed3ad407603653c817525a128139
neternefer <alkarinqe@yahoo.com>
2020-04-14 13:38:07 +0200

    Add app.js file structure

---
+++ js/app.js
@@ -0,0 +1,42 @@
+/**
+ * Define Global Variables
+ *
+*/
+const menuList = document.getElementById("dynamic-menu-list");
+const linkedSections = document.getElementsByClassName("linked");
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
